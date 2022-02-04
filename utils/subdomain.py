f8546309b59c0514b2983482143bd7c5b83ace95
orange68 <61174657+yulingy68@users.noreply.github.com>
2022-02-04 12:45:25 -0800

    Update subdomain.py

--- utils/subdomain.py
+++ utils/subdomain.py
@@ -1,5 +1,5 @@
 def saveSubdomains(subDomainsDict):
-    f = open("Subdomain_List.txt","w")
+    f = open("subdomain_list.txt","w")
     for subDomain in sorted(subDomainsDict.keys()):
         f.write(f"{subDomain}, {subDomainsDict[subDomain]}\n")
     f.close()
